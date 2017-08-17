ec984a08905ee6d7406c353fef8e5a35f3b86853
ArunKumar-P <reacharun@email.com>
2017-08-18 00:16:39 +0530

    Changes for Module P5. Added server.js

--- server.js
+++ server.js
@@ -218,6 +218,7 @@ app.get('style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+//Added in Module P4
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
